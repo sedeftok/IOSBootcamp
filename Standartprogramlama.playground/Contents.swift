3ff351bd63aeaf93932e8c23eab63757b5c1316c
sedef tok <sedeftok03@gmail.com>
2024-03-11 23:45:12 +0300

    Day1

---
+++ Standartprogramlama.playground/Contents.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+var yas  = 19
+var isim = "Işıl"
+
+if yas >= 18 {
+    print("Reşitsiniz")
+}else{
+    print("Reşit değilsiniz")
+}
+
+if isim == " Eren "{
+    print("Merhaba Eren")
+}else{
+    print("Tanınmayan kişi")
+}
